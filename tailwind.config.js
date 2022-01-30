76427d63a379a6c22a2ef6c80944c28fbbffd3f6
Mike <Mikephse@gmail.com>
2022-01-30 01:41:06 -0500

    Finish create Register form using Reactive form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   purge: {
     content: ["./src/**/*.{html,ts}"],
+    options: {
+      safelist: ["bg-blue-400", "bg-green-400", "bg-red-400"],
+    },
   },
   content: [],
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {
+      opacity: ["disabled"],
+      backgroundColor: ["disabled"],
+    },
   },
   plugins: [],
 };
