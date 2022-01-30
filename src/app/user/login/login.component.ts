9e9988385189af04dc66328eb87bd751727e341a
Mike <Mikephse@gmail.com>
2022-01-30 02:31:53 -0500

    Finish create login form using Template Form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  credentials = {
+    email: '',
+    password: '',
+  };
+  // showAlert = false;
+  // alertMessage = 'Please wait! Your account is being created.';
+  // alertColor = 'blue';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  login = () => {
+    // this.showAlert = true;
+    // this.alertMessage = 'Please wait! Your account is being created.';
+    // this.alertColor = 'blue';
+    console.log('login object: ', this.credentials);
+  };
 }
