959a0abc71394de66f041a59797324bed215f2bc
Mike <Mikephse@gmail.com>
2022-05-12 00:11:23 -0400

    bugfix screenshot reference

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -52,7 +52,9 @@ export class ClipService {
 
   async deleteClip(clip: Clip) {
     const clipRef = this.storage.ref(`clips/${clip.fileName}`);
-    const screenshotRef = this.storage.ref(`clips/${clip.screenshotFileName}`);
+    const screenshotRef = this.storage.ref(
+      `screenshots/${clip.screenshotFileName}`
+    );
     await screenshotRef.delete();
     await clipRef.delete();
     await this.clipCollection.doc(clip.documentId).delete();
