36759a8febff05633e8626656bec099179583278
Mike <Mikephse@gmail.com>
2022-05-13 23:27:17 -0400

    Update playing the correct video from database

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -7,25 +7,31 @@ import {
 } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import videojs from 'video.js';
+import Clip from '../models/clip.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.css'],
   encapsulation: ViewEncapsulation.None,
+  providers: [DatePipe],
 })
 export class ClipComponent implements OnInit {
-  id = '';
   @ViewChild('videoPlayer', { static: true }) target?: ElementRef;
   player?: videojs.Player;
+  clip?: Clip;
 
   constructor(public activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    // this.id = this.activatedRoute.snapshot.params.id;
     this.player = videojs(this.target?.nativeElement);
-    this.activatedRoute.params.subscribe((params: Params) => {
-      this.id = params.id;
+    this.activatedRoute.data.subscribe((data) => {
+      this.clip = data.clip as Clip;
+      this.player?.src({
+        src: this.clip.clipUrl,
+        type: 'video/mp4',
+      });
     });
   }
 }
